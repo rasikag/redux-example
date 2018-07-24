f217723d517fc0ded4e392d51eeb66e95fb2cba5
rasikag <rasikagayan90@gmail.com>
2018-07-24 22:44:54 +0800

    Adding Subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -31,6 +31,10 @@ const store = createStore(rootReducer);
 
 console.log(store.getState());
 
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
 store.dispatch({ type: 'INC_COUNTER' });
 
 store.dispatch({ type: 'ADD_COUNTER', value: 10});
