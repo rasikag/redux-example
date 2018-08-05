bf2f658f39cf4512d31d83324fe4e5e8f36efa3c
rasikag <rasikagayan90@gmail.com>
2018-08-05 14:54:20 +0800

    Handling Asynchronous Code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -31,15 +31,21 @@ export const subtract = (value) => {
     }
 }
 
-export const storeResult = (res) => {
-
-    setTimeout(()=> {
-        
-    }, 2000);
+export const saveResult = ( res ) => {
     return {
         type: STORE_RESULT,
         result: res
     }
+}
+
+export const storeResult = (res) => {
+    return dispatch => {
+        setTimeout(()=> {
+            dispatch(saveResult(res));
+        }, 2000);
+    }
+    
+    
 }
 
 export const deleteResult = (resElId) => {
